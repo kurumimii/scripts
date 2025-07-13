331533ca5bcb3dcf2b3adbdeea42ac7a2b94414d
kurumimii <alfiesalig@gmail.com>
2025-07-13 16:28:30 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet("https://pastefy.app/BEVqbxGr/raw"))()
+local Spawner = loadstring(game:HttpGet("https://codeberg.org/GrowAFilipino/GrowAGarden/raw/branch/main/Spawner.lua"))()
+Spawner.Load()
